7ba01198ce9bcf954954be3159784b500d38151b
A7MOGERY <49486327+A7MOGERY@users.noreply.github.com>
2020-07-06 12:30:47 +0800

    master

---
+++ Test/Profile Card/main.js
@@ -0,0 +1,16 @@
+const img = document.querySelector('.image');
+const hover = document.querySelector('.hover');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+
+function show() {
+    hover.classList.add('active');
+    modal.classList.add('show');
+}
+
+function hide() {
+    hover.classList.remove('active');
+    modal.classList.remove('show');
+}
+img.addEventListener("click", show);
+close.addEventListener("click", hide);
